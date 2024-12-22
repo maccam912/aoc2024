14164ed1c555d6ee9beaa7d1bc0676a9d1a8de3d
Matt Koski <maccam912@gmail.com>
2024-12-22 10:11:17 -0600

    layer 1 done

--- src/day21.rs
+++ src/day21.rs
@@ -1,299 +1,100 @@
 use crate::Solution;
 
+struct Coordinate {
+    x: i32,
+    y: i32,
+}
+
+struct KeypadRobot {
+    pos: Coordinate,
+    actions: Vec<char>,
+    presses: Vec<char>,
+}
+
+impl KeypadRobot {
+    fn new() -> Self {
+        KeypadRobot {
+            pos: Coordinate { x: 2, y: 3 }, // Start at 2,3 for the A button
+            actions: Vec::new(),
+            presses: Vec::new(),
+        }
+    }
+
+    fn do_action(&mut self, action: char) {
+        match action {
+            '^' => self.pos.y -= 1,
+            'v' => self.pos.y += 1,
+            '<' => self.pos.x -= 1,
+            '>' => self.pos.x += 1,
+            'A' => {
+                // This pushes the button you are on. Given the coordinates you're at will determine what to add to presses
+                // Keypad is in this layout with 0,0 at top left:
+                // 7 8 9
+                // 4 5 6
+                // 1 2 3
+                //   0 A (blank left of zero)
+                match (self.pos.x, self.pos.y) {
+                    (0, 0) => self.presses.push('7'),
+                    (1, 0) => self.presses.push('8'),
+                    (2, 0) => self.presses.push('9'),
+                    (0, 1) => self.presses.push('4'),
+                    (1, 1) => self.presses.push('5'),
+                    (2, 1) => self.presses.push('6'),
+                    (0, 2) => self.presses.push('1'),
+                    (1, 2) => self.presses.push('2'),
+                    (2, 2) => self.presses.push('3'),
+                    (1, 3) => self.presses.push('0'),
+                    (2, 3) => self.presses.push('A'),
+                    _ => panic!("Invalid position for A button: x={}, y={}", self.pos.x, self.pos.y),
+                }
+            }
+            _ => panic!("Invalid action: {}", action),
+        }
+        self.actions.push(action);
+    }
+}
+
 pub struct Day21;
 
 impl Solution for Day21 {
     fn part1(&self, input: &str) -> String {
-        part1(input)
+        // TODO: Implement solution
+        "Not implemented".to_string()
     }
 
     fn part2(&self, input: &str) -> String {
-        part2(input)
+        // TODO: Implement solution
+        "Not implemented".to_string()
     }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Position {
-    row: i32,
-    col: i32,
-}
-
-#[derive(Debug, Clone)]
-struct NumericKeypadRobot {
-    position: Position,
-    sequence: Vec<char>,
-}
-
-#[derive(Debug, Clone)]
-struct DirectionalKeypadRobot {
-    position: Position,
-    moves: Vec<char>,
-    button_presses: Vec<char>,
-}
-
-impl NumericKeypadRobot {
-    fn new() -> Self {
-        Self {
-            position: Position { row: 3, col: 2 }, // Starting at 'A'
-            sequence: Vec::new(),
-        }
-    }
-
-    fn is_valid_position(&self, pos: Position) -> bool {
-        match (pos.row, pos.col) {
-            (0..=2, 0..=2) => true,  // Regular grid 7-9, 4-6, 1-3
-            (3, 1..=2) => true,      // 0 and A
-            _ => false,
-        }
-    }
-
-    fn get_button_at(&self, pos: Position) -> Option<char> {
-        if !self.is_valid_position(pos) {
-            return None;
-        }
-
-        match (pos.row, pos.col) {
-            (0, col) => Some(char::from_digit((7 + col) as u32, 16).unwrap()),
-            (1, col) => Some(char::from_digit((4 + col) as u32, 16).unwrap()),
-            (2, col) => Some(char::from_digit((1 + col) as u32, 16).unwrap()),
-            (3, 1) => Some('0'),
-            (3, 2) => Some('A'),
-            _ => None,
-        }
-    }
-
-    fn get_position_for_button(&self, button: char) -> Option<Position> {
-        match button {
-            '7'..='9' => Some(Position { row: 0, col: (button as i32 - '7' as i32) }),
-            '4'..='6' => Some(Position { row: 1, col: (button as i32 - '4' as i32) }),
-            '1'..='3' => Some(Position { row: 2, col: (button as i32 - '1' as i32) }),
-            '0' => Some(Position { row: 3, col: 1 }),
-            'A' => Some(Position { row: 3, col: 2 }),
-            _ => None,
-        }
-    }
-}
-
-impl DirectionalKeypadRobot {
-    fn new() -> Self {
-        Self {
-            position: Position { row: 0, col: 2 }, // Starting at 'A'
-            moves: Vec::new(),
-            button_presses: Vec::new(),
-        }
-    }
-
-    fn is_valid_position(&self, pos: Position) -> bool {
-        match (pos.row, pos.col) {
-            (0, 1..=2) => true,     // ^ and A
-            (1, 0..=2) => true,     // <, v, >
-            _ => false,
-        }
-    }
-
-    fn find_path_to(&mut self, target: Position) -> Vec<char> {
-        let mut path = Vec::new();
-        let mut current = self.position;
-
-        // If we're already at the target, just press A
-        if current == target {
-            path.push('A');
-            return path;
-        }
-
-        // First move vertically to align with row if needed
-        while current.row != target.row {
-            if current.row < target.row {
-                path.push('v');
-                current.row += 1;
-            } else {
-                path.push('^');
-                current.row -= 1;
-            }
-        }
-
-        // Then move horizontally
-        while current.col != target.col {
-            if current.col < target.col {
-                path.push('>');
-                current.col += 1;
-            } else {
-                path.push('<');
-                current.col -= 1;
-            }
-        }
-
-        // Add the final button press
-        path.push('A');
-        
-        // Update robot's position
-        self.position = current;
-        
-        path
-    }
-
-    fn press_button(&mut self) {
-        self.button_presses.push('A');
-    }
-
-    fn execute_moves(&mut self, moves: Vec<char>) {
-        for m in moves {
-            self.moves.push(m);
-            match m {
-                '^' => self.position.row -= 1,
-                'v' => self.position.row += 1,
-                '<' => self.position.col -= 1,
-                '>' => self.position.col += 1,
-                'A' => self.press_button(),
-                _ => (),
-            }
-        }
-    }
-}
-
-fn find_shortest_sequence(code: &str) -> (String, usize) {
-    let keypad_robot = NumericKeypadRobot::new();
-    let mut intermediate_robot1 = DirectionalKeypadRobot::new();
-    let mut intermediate_robot2 = DirectionalKeypadRobot::new();
-    let mut final_sequence = String::new();
-
-    // Process each character in the code
-    for c in code.chars() {
-        // Get the target position on the numeric keypad
-        let target_pos = keypad_robot.get_position_for_button(c).unwrap();
-        
-        // Find path for robot1 to get to the position to press the directional buttons
-        let mut robot1_sequence = String::new();
-        let mut current_pos = intermediate_robot1.position;
-        
-        // For each button press needed by robot2
-        let robot2_path = intermediate_robot2.find_path_to(target_pos);
-        for move_dir in robot2_path {
-            // Find the position of the direction button on robot1's keypad
-            let dir_pos = match move_dir {
-                '^' => Position { row: 0, col: 1 },
-                'v' => Position { row: 1, col: 1 },
-                '<' => Position { row: 1, col: 0 },
-                '>' => Position { row: 1, col: 2 },
-                'A' => Position { row: 0, col: 2 },
-                _ => continue,
-            };
-            
-            // Find path to the button and press it
-            let path = intermediate_robot1.find_path_to(dir_pos);
-            for p in path {
-                robot1_sequence.push(p);
-                current_pos = match p {
-                    '^' => Position { row: current_pos.row - 1, col: current_pos.col },
-                    'v' => Position { row: current_pos.row + 1, col: current_pos.col },
-                    '<' => Position { row: current_pos.row, col: current_pos.col - 1 },
-                    '>' => Position { row: current_pos.row, col: current_pos.col + 1 },
-                    _ => current_pos,
-                };
-            }
-            robot1_sequence.push('A');
-        }
-        
-        // Add the sequence for this character
-        final_sequence.push_str(&robot1_sequence);
-    }
-
-    let sequence_len = final_sequence.len();
-    (final_sequence, sequence_len)
-}
-
-pub fn part1(input: &str) -> String {
-    let codes = input.lines().collect::<Vec<_>>();
-    let mut total_complexity = 0usize;
-
-    for code in codes {
-        let (_, length) = find_shortest_sequence(code);
-        let numeric_part = code.trim_start_matches('0')
-            .trim_end_matches('A')
-            .parse::<usize>()
-            .unwrap();
-        
-        total_complexity += length * numeric_part;
-    }
-
-    total_complexity.to_string()
-}
-
-pub fn part2(_input: &str) -> String {
-    "Not implemented".to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::read_input;
 
     #[test]
-    fn test_robot2_sequence() {
-        let keypad_robot = NumericKeypadRobot::new();
-        let mut robot2 = DirectionalKeypadRobot::new();
-        
-        // Test for pressing "0"
-        let target_pos = keypad_robot.get_position_for_button('0').unwrap();
-        let moves = robot2.find_path_to(target_pos);
-        let sequence: String = moves.into_iter().collect();
-        println!("Sequence for '0': {}", sequence);
-        
-        // Test for pressing "2"
-        let target_pos = keypad_robot.get_position_for_button('2').unwrap();
-        let moves = robot2.find_path_to(target_pos);
-        let sequence: String = moves.into_iter().collect();
-        println!("Sequence for '2': {}", sequence);
-        
-        // Test for pressing "9"
-        let target_pos = keypad_robot.get_position_for_button('9').unwrap();
-        let moves = robot2.find_path_to(target_pos);
-        let sequence: String = moves.into_iter().collect();
-        println!("Sequence for '9': {}", sequence);
-        
-        // Test for pressing "A"
-        let target_pos = keypad_robot.get_position_for_button('A').unwrap();
-        let moves = robot2.find_path_to(target_pos);
-        let sequence: String = moves.into_iter().collect();
-        println!("Sequence for 'A': {}", sequence);
+    fn test_part1_sample() {
+        let input = read_input(21, true);
+        assert_eq!(Day21.part1(&input), "Not implemented");
     }
 
     #[test]
-    fn test_full_sequence() {
-        let keypad_robot = NumericKeypadRobot::new();
-        let mut robot2 = DirectionalKeypadRobot::new();
-        let mut full_sequence = String::new();
-
-        // Press "0"
-        let target_pos = keypad_robot.get_position_for_button('0').unwrap();
-        let moves = robot2.find_path_to(target_pos);
-        full_sequence.extend(moves.iter());
-
-        // Press "2"
-        let target_pos = keypad_robot.get_position_for_button('2').unwrap();
-        let moves = robot2.find_path_to(target_pos);
-        full_sequence.extend(moves.iter());
-
-        // Press "9"
-        let target_pos = keypad_robot.get_position_for_button('9').unwrap();
-        let moves = robot2.find_path_to(target_pos);
-        full_sequence.extend(moves.iter());
-
-        // Press "A"
-        let target_pos = keypad_robot.get_position_for_button('A').unwrap();
-        let moves = robot2.find_path_to(target_pos);
-        full_sequence.extend(moves.iter());
-
-        println!("Full sequence for '029A': {}", full_sequence);
-        // One of the valid sequences from the problem description is: <A^A>^^AvvvA
-        assert!(full_sequence.len() <= 11, "Sequence should be no longer than the example sequence");
+    fn test_part2_sample() {
+        let input = read_input(21, true);
+        assert_eq!(Day21.part2(&input), "Not implemented");
     }
-    
-    // Commenting out the original test temporarily
-    /*
+
     #[test]
-    fn test_part1() {
-        let input = "029A\n980A\n179A\n456A\n379A";
-        assert_eq!(part1(input), "126384");
+    fn test_robot_actions_and_presses() {
+        let mut robot = KeypadRobot::new();
+        let actions = "<A^A>^^AvvvA";
+        
+        for c in actions.chars() {
+            robot.do_action(c);
+        }
+
+        assert_eq!(robot.actions, actions.chars().collect::<Vec<char>>());
+        assert_eq!(robot.presses, vec!['0', '2', '9', 'A']);
     }
-    */
 }
